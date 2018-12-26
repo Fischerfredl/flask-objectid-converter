0cba71305bd48101c3f1c650b9c9d68276b481fb
Iddan Aaronsohn <mail@aniddan.com>
2018-12-26 14:22:43 +0200

    Make Base64 ObjectId Converter a different class

--- flask_objectid_converter/__init__.py
+++ flask_objectid_converter/__init__.py
@@ -5,25 +5,24 @@ from itsdangerous import base64_encode, base64_decode
 
 
 class ObjectIDConverter(BaseConverter):
-    base64: bool
-    """
-    Flags wether received and converted ObjectId strings should be in Base64
-    """
-        
-    def __init__(self, base64 = False):
-        self.base64 = base64
-    
     def to_python(self, value):
         try:
-            if self.base64:
-                value = base64_decode(value)
             return ObjectId(value)
         
         except (InvalidId, ValueError, TypeError):
             raise ValidationError()
 
     def to_url(self, value):
-        if self.base64:
-            return base64_encode(value.binary).decode('utf-8')
-        else:
-            return str(value)
+        return str(value)
+
+
+class Base64ObjectIDConverter(BaseConverter):
+    def to_python(self, value):
+        try:
+            return ObjectId(base64_decode(value))
+        
+        except (InvalidId, ValueError, TypeError):
+            raise ValidationError()
+
+    def to_url(self, value):
+        return base64_encode(value.binary).decode('utf-8')
