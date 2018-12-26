6063269976dc4b3aa7e013bcf23d3bec2b90c060
Iddan Aaronsohn <mail@aniddan.com>
2018-12-26 13:51:04 +0200

    Optional base64 support

--- flask_objectid_converter/__init__.py
+++ flask_objectid_converter/__init__.py
@@ -5,11 +5,25 @@ from itsdangerous import base64_encode, base64_decode
 
 
 class ObjectIDConverter(BaseConverter):
+    base64: bool
+    """
+    Flags wether received and converted ObjectId strings should be in Base64
+    """
+        
+    def __init__(self, base64 = False):
+        self.base64 = base64
+    
     def to_python(self, value):
         try:
-            return ObjectId(base64_decode(value))
+            if self.base64:
+                value = base64_decode(value)
+            return ObjectId(value)
+        
         except (InvalidId, ValueError, TypeError):
             raise ValidationError()
 
     def to_url(self, value):
-        return base64_encode(value.binary).decode('utf-8')
+        if self.base64:
+            return base64_encode(value.binary).decode('utf-8')
+        else:
+            return str(value)
