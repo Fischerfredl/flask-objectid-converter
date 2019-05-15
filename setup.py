1eed5165bad1bfa526bf98f10da1c174e8cb9c70
Alfred Melch <code@melch.pro>
2019-05-15 14:49:41 +0200

    Remove py3.7 from pipeline. (Prematurely added)

--- setup.py
+++ setup.py
@@ -21,8 +21,7 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7'
+        'Programming Language :: Python :: 3.6'
     ],
     keywords=['flask', 'bson', 'pymongo', 'objectid', 'converter'],
     packages=find_packages(exclude=['tests.*', 'tests']),
